0a6c118c3b1460738b15c5b21d9eaa2e37018ccf
Esthyehenny <hennyethan98@gmail.com>
2022-12-08 15:55:36 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ *main - Starting point for everything else
+ *
+ *Return: Always 0 (Success)
+ */
+int main (void)
+{
+  puts("Programming is like building a multilingual puzzle\n");
+  return (0);
+}
