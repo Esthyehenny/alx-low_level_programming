2fd5da890e73d0ae1b1634c82cccafb561bc14c3
Esthyehenny <hennyethan98@gmail.com>
2022-12-14 06:25:55 -0800

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * main - jack_bauer - Write a function that prints every
- * minute of the day of Jack Bauer,
+ * jack_bauer - Write a function that prints every
+ * minute of the day of Jack Bauer
  * starting from 00:00 to 23:59
  */
 
