20dd9f36ec5cac82352640e5ac5818fa8c3f42a7
Esthyehenny <hennyethan98@gmail.com>
2022-12-19 19:52:11 -0800

    102-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -11,14 +11,14 @@ int main(void)
 {
 long int l, m = 1, n = 2, sum = 0;
 
-for (l = 0; l < 49; l++)
+for (l = 0; l < 50; l++)
 {
 printf("%ld, ", m);
 sum = m + n;
 m = n;
 n = sum;
 
-if (l == 48)
+if (l == 49)
 printf("%ld\n", l);
 }
 
